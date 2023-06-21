7e06ee77d92c09fae3d4e9cc93a37425e0a3eb04
Leo Donson <donson.lee99@gmail.com>
2023-06-21 19:25:48 +1000

    Adding email logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,9 @@
 import asyncHandler from "express-async-handler";
 import UserModel from "../models/userModel.js";
 import genToken from "../utils/generateToken.js";
+import TokenModel from "../models/tokenModel.js";
+import crypto from "node:crypto";
+import sendEmail from "../utils/sendEmail.js";
 
 /*REGISTER USER CTRL/FUNCTION STARTS HERE.*/
 const registerUser = asyncHandler(async (req, res) => {
@@ -140,8 +143,102 @@ const changePassword = asyncHandler(async (req, res) => {
 
 /*FORGOT PASSWORD CTRL/FUNCTION STARTS HERE.*/
 const forgotPassword = asyncHandler(async (req, res) => {
-  res.send("forgot password?..");
+  const { email } = req.body;
+  const user = await UserModel.findOne({ email });
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found.");
+  }
+  //delete previous token if exist
+  let tokenExist = await TokenModel.findOne(user._id);
+  if (tokenExist) {
+    await tokenExist.deleteOne();
+  }
+  //create reset token
+  let resetToken = crypto.randomBytes(32).toString("hex") + user._id;
+
+  //hash the token
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  //save the token to the BD
+  const token = await new TokenModel({
+    userId: user._id,
+    token: hashedToken,
+    createdAt: Date.now(),
+    expiresAt: Date.now() + 30 * (60 * 1000), // 30 mins
+  });
+  token.save();
+
+  //construct reset url
+  const resetUrl = `${process.env.FRONTEND_URL}/resetpassword/${hashedToken}`;
+
+  //email message
+  const message = `
+  <h1>Hello,${user.firstName}</h1>
+  <p>You have requested for password reset.</p>
+  <p>Please use the url linked below to reset your password.</p>
+  <p>This url link is valid for only 30 minutes.</p>
+  <a href=${resetUrl} clicktracking=off>${resetUrl}</a>
+  
+  <p>Best regards...</p>
+  <p>Niugini Market Team</p>
+  `;
+  const subject = "Password Reset Request";
+  const mail_to = user.email;
+  const mail_from = process.env.EMAIL_USER;
+  const noReply = `<p>Sender does not support replies.</p>`;
+
+  try {
+    await sendEmail(subject, message, mail_to, mail_from, noReply);
+    res.status(200).json({ success: true, message: "Reset email sent." });
+  } catch (error) {
+    res.status(500);
+    throw new Error("Email not sent, please try again later.");
+  }
 });
 /*FORGOT PASSWORD CTRL/FUNCTION ENDS HERE.*/
 
-export { registerUser, loginUser, logoutUser, changePassword, forgotPassword };
+/*RESET PASSWORD CTRL/FUNCTION STARTS HERE.*/
+const resetPassword = asyncHandler(async (req, res) => {
+  const { password } = req.body;
+  const { resetToken } = req.params;
+
+  //hash token, then compare token in db
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  //find token in db
+  const userToken = await TokenModel.findOne({
+    token: hashedToken,
+    expiresAt: { $gt: Date.now() },
+  });
+
+  if (!userToken) {
+    res.status(404);
+    throw new Error("Invalid or expired token");
+  }
+
+  const user = await UserModel.findOne({ _id: userToken.userId });
+  user.password = password;
+  await user.save();
+  res.status(200).json({
+    success: true,
+    message: "Password reset success. Please use this password to login.",
+  });
+});
+/*RESET PASSWORD CTRL/FUNCTION ENDS HERE.*/
+
+export {
+  registerUser,
+  loginUser,
+  logoutUser,
+  changePassword,
+  forgotPassword,
+  resetPassword,
+};
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,7 +1,7 @@
-import nodemailer from "nodemailer";
+import nodeMailer from "nodemailer";
 
-const sendEmail = async (suject, message, mail_to, mail_from, reply_to) => {
-  const transporter = nodemailer.createTransport({
+const sendEmail = async (subject, message, mail_to, mail_from, reply_to) => {
+  const transporter = nodeMailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: 587,
     auth: {
@@ -12,7 +12,22 @@ const sendEmail = async (suject, message, mail_to, mail_from, reply_to) => {
       rejectUnauthorized: false,
     },
   });
-  const options = {};
+
+  const options = {
+    from: mail_from,
+    to: mail_to,
+    replyTo: reply_to,
+    subject: subject,
+    html: message,
+  };
+  //send email
+  transporter.sendMail(options, function (error, success) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(success);
+    }
+  });
 };
 
 export default sendEmail;
